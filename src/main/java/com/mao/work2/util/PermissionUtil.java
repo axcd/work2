4c8fde58821123e0fd11c5957440d506e744536d
axcd <maoyuyin@126.com>
2020-09-14 18:43:00 +0800

    6.0读写权限申请

--- src/main/java/com/mao/work2/util/PermissionUtil.java
+++ src/main/java/com/mao/work2/util/PermissionUtil.java
@@ -4,7 +4,8 @@ import android.Manifest;
 import android.app.Activity;
 import android.content.pm.PackageManager;
 
-public class PermissionUtil {
+public class PermissionUtil
+{
 
     // Storage Permissions
     private static final int REQUEST_EXTERNAL_STORAGE = 1;
@@ -14,15 +15,20 @@ public class PermissionUtil {
     };
 
     //android6.0之后要动态获取权限
-    public static void requestPermission(Activity activity) {
-        int permission = activity.checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE);
-        //检测是否有写的权限
-        if (permission != PackageManager.PERMISSION_GRANTED) {
-            // 没有写的权限，去申请写的权限，会弹出对话框
-            activity.requestPermissions(
-				PERMISSIONS_STORAGE,
-				REQUEST_EXTERNAL_STORAGE
-            );
-        }
-    }
+    public static void requestPermission(Activity activity)
+	{
+		if (android.os.Build.VERSION.SDK_INT >= 23)
+		{
+			int permission = activity.checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE);
+			//检测是否有写的权限
+			if (permission != PackageManager.PERMISSION_GRANTED)
+			{
+				// 没有写的权限，去申请写的权限，会弹出对话框
+				activity.requestPermissions(
+					PERMISSIONS_STORAGE,
+					REQUEST_EXTERNAL_STORAGE
+				);
+			}
+		}
+	}
 }
